41fd90463619399acd1922bbfbcf92bac07fa7ac
Denis-Hodzic <denis.hodzic@etu.univ-smb.fr>
2026-02-23 17:09:07 +0100

    Film db fait

---
+++ Tp4_codeFirst/Models/EntityFramework/Film.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Intrinsics.X86;
+
+namespace Tp4_codeFirst.Models.EntityFramework
+{
+    [Table("t_e_film_flm")]
+    public partial class Film
+    {
+        [Key]
+        [Column("flm_id")]
+        public int FilmId { get; set; }
+
+        [Column("flm_titre")]
+        [StringLength(100)]
+        public string Titre { get; set; } = null!;
+
+        [Column("flm_resume")]
+        public string? Resume { get; set; }
+
+        [Column("flm_datesortie")]
+        public DateTime DateSortie { get; set; }
+
+        [Column("flm_duree")]
+        public decimal Duree { get; set; }
+
+        [Column("flm_genre")]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [InverseProperty(nameof(Notation.NotesFilm))]
+        public virtual ICollection<Avi> Avis { get; set; } = new List<Avi>();
+    }
+}
