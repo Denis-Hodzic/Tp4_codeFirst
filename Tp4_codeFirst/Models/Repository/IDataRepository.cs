7f09255076cd7fbe1b8cf0037387274240802789
Denis-Hodzic <denis.hodzic@etu.univ-smb.fr>
2026-02-24 23:10:04 +0100

    ajout de UtilisateurManager

---
+++ Tp4_codeFirst/Models/Repository/IDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tp4_codeFirst.Models.Repository
+{
+    public interface IDataRepository<TEntity>
+    {
+        ActionResult<IEnumerable<TEntity>> GetAll();
+        ActionResult<TEntity> GetById(int id);
+        ActionResult<TEntity> GetByString(string str);
+        void Add(TEntity entity);
+        void Update(TEntity entityToUpdate, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
